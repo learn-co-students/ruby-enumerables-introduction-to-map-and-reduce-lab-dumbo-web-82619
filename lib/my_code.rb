217cebe51ec455ef9a4068c3ff6e0908b3892bf0
Jonathan Koss <jonathan_koss@yahoo.com>
2019-06-26 21:59:47 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,11 +15,17 @@ def map_to_square(source_array)
   source_array.map {|element| element ** 2}
 end 
 
-def reduce_to_total(source_array, starting_point)
-  source_array.reduce(starting_point) {|total, number| total + number}
+def reduce_to_total(source_array, starting_point = 0)
+  source_array.reduce(starting_point){|total, number| total + number}
 end 
 
 def reduce_to_all_true(source_array)
-  
+  source_array.all? {|element| !!element}
 end 
+
+def reduce_to_any_true(source_array)
+  source_array.any? {|element| !!element}
+end 
+
+
 
