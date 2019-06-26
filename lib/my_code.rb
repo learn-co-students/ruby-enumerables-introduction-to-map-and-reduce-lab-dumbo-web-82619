975cc434b4e2c9a32ccc58ba94399c8274c28325
Jonathan Koss <jonathan_koss@yahoo.com>
2019-06-26 03:01:16 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # My Code here....
+def map_to_negativize(source_array)
+  source_array.map{|element| element * -1}  
+end 
+
+def map_to_no_change(source_array)
+    source_array.map{|element| element}
+end 
+
+def map_to_double(source_array)
+  source_array.map {|element| element * 2}
+end 
+
+def map_to_square(source_array) 
+  source_array.map {|element| element ** 2}
+end 
+
+def reduce_to_total(source_array, starting_point)
+  source_array.reduce(starting_point) {|total, number| total + number}
+end 
+
+def reduce_to_all_true(source_array)
+  
+end 
+
