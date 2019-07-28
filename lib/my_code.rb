18958bf7c0c91b69597bab53e379b2810e543f49
Benny Louie <bennylouie1412@gmail.com>
2019-07-28 19:13:09 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -33,24 +33,41 @@ def map_to_square(source_array)
   return squared_array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   i = 0 
-  running_point = 0
-  if starting_point
-    running_point = starting_point
-    while i < source_array.size do
-      running_point += source_array[i]
-      i += 1
-    return running_point
-  end
+  running_total = 0
+  running_total += starting_point
   while i < source_array.size do
-    running_point += source_array[i]
-    i += -1
-    return running_point
+    running_total += source_array[i]
+    i += 1
   end
+  return running_total
 end
-  
-  
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.size do
+    if source_array[i] == false 
+      return false
+    end
+     i += 1 
+  end 
+  return true
+end
+
+      
+def reduce_to_any_true(source_array)
+  i = 0 
+  while i < source_array.size do
+    if source_array[i] == true 
+      return true 
+    end 
+    i += 1 
+  end 
+  return false 
+end 
+      
+      
   
   
   
